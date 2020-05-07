4367ef603fbbba69ac14bf2b59cd999ac1c8ce60
Christopher Lewis <cjoellewis@yahoo.com>
2020-05-07 01:26:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,12 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    song = self.find_by_name || self.create_by_name
-    song
+    #binding.pry
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
   end
  
 end

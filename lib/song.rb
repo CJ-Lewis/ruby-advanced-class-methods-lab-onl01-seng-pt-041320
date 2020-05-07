7e35439837aa81a166b39f5e68ec7f07811c469d
Christopher Lewis <cjoellewis@yahoo.com>
2020-05-07 01:19:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
- 
+
   def self.all
     @@all
   end
- 
+
   def save
     self.class.all << self
   end
-  
+
   def self.create
     song = self.new
-    self.save
+    song.save
     song
   end
  
